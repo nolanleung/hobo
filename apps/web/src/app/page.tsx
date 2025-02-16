4ab2961349adff92024675ab263b24e34e2460e3
Nolan Leung <leung.nolan@gmail.com>
2025-02-16 17:23:35 -0500

    feat: add shadcn

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-background flex h-screen w-screen items-center justify-center">
+    <div className="bg-background text-foreground flex h-screen w-screen items-center justify-center">
       <h1 className="text-3xl">Hello World</h1>
     </div>
   );
